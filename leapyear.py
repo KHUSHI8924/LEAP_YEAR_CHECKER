22f1f6e4a36b3e52b6aacf611f6dc11a6d7c01b3
KHUSHI8924 <khushidriven@gmail.com>
2025-10-16 19:14:28 +0530

    add comments

--- leapyear.py
+++ leapyear.py
@@ -1,5 +1,12 @@
 year=int(input("Enter the year:"))
 if (((year%4==0) and (year%100!=0)) or (year%400==0)):
+    """"
+    if a year is multiple of 4 then it is a leap year
+    """
     print("{0} is a leap year!!".format(year))
+    """"
+    printing the output
+    """
 else:
     print("{0} is not a leap year!!".format(year))
+    
